b311452fe62aa394df0e6577d9ebea98225ceb88
Elias Pinheiro <pinheirinho2004@gmail.com>
2023-12-21 17:07:40 -0300

    routes '/users' and '/users/new' created

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { PrismaClient } from "@prisma/client";
 import Controller from "./Controller";
 
 export default class UserController extends Controller {
@@ -8,10 +9,37 @@ export default class UserController extends Controller {
     }
 
     private initializeRoutes() {
-        this.router.get("/users", this.getusers)
+        this.router.get("/users", this.getUsers)
+        this.router.post("/users/new", this.createUser)
     }
 
-    private getusers(req: Request, res: Response) {
-        res.send({ status: "ok" })
+    private async getUsers(req: Request, res: Response) {
+        const p = new PrismaClient()
+
+        const users = await p.user.findMany({
+            select: {
+                id: true, firstName: true, secondName: true, email: true
+            }
+        })
+
+        p.$disconnect()
+        res.send(users)
+    }
+
+    private async createUser(req: Request, res: Response) {
+        const { body } = req
+
+        const p = new PrismaClient()
+        const user = await p.user.create({
+            data: {
+                firstName: body.firstName,
+                secondName: body.secondName,
+                email: body.email,
+                password: body.password
+            }
+        })
+
+        p.$disconnect()
+        res.send({ user })
     }
 }
