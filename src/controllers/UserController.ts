30fb405e8899ee911e77ce571a851d5b8e488215
Elias Pinheiro <pinheirinho2004@gmail.com>
2023-12-23 12:13:59 -0300

    route '/users/:id' created

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -11,11 +11,12 @@ export default class UserController extends Controller {
 
     private initializeRoutes() {
         this.router.get("/users", this.getUsers)
+        this.router.get("/users/:id", this.getUserById)
+        this.router.delete("/users/:id/delete")
         this.router.post("/users/new", UserValidator.createUser, this.createUser)
     }
 
     private async getUsers(req: Request, res: Response) {
-
         const users = await prisma.user.findMany({
             select: {
                 id: true, firstName: true, secondName: true, email: true
@@ -23,6 +24,26 @@ export default class UserController extends Controller {
         })
 
         res.send(users)
+    }
+
+    private async getUserById(req: Request, res: Response) {
+        const id = Number.parseInt(req.params.id)
+
+        if (isNaN(id)) {
+            return res.status(400).send({ error: "user id must be a number. /users/:id[number]" })
+        }
+
+
+        const user = await prisma.user.findUnique({
+            where: { id },
+            select: { id: true, firstName: true, secondName: true, email: true }
+        })
+
+        if (!user) {
+            return res.status(404).send({ error: "User not found" })
+        }
+
+        res.send(user)
     }
 
     private async createUser(req: Request, res: Response) {
@@ -33,5 +54,11 @@ export default class UserController extends Controller {
         })
 
         res.send({ user })
+    }
+
+    private async deleteUser(req: Request, res: Response) {
+        const id = req.query.id
+        console.log(id)
+        res.send({ status: "ok" })
     }
 }
