ae3b6a1970187d389d2c4d2cd2ce8ffdc69b5670
Elias Pinheiro <pinheirinho2004@gmail.com>
2023-12-23 12:32:35 -0300

    route '/users/:id/delete' created

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -12,7 +12,7 @@ export default class UserController extends Controller {
     private initializeRoutes() {
         this.router.get("/users", this.getUsers)
         this.router.get("/users/:id", this.getUserById)
-        this.router.delete("/users/:id/delete")
+        this.router.delete("/users/:id/delete", this.deleteUser)
         this.router.post("/users/new", UserValidator.createUser, this.createUser)
     }
 
@@ -32,7 +32,6 @@ export default class UserController extends Controller {
         if (isNaN(id)) {
             return res.status(400).send({ error: "user id must be a number. /users/:id[number]" })
         }
-
 
         const user = await prisma.user.findUnique({
             where: { id },
@@ -57,8 +56,27 @@ export default class UserController extends Controller {
     }
 
     private async deleteUser(req: Request, res: Response) {
-        const id = req.query.id
-        console.log(id)
-        res.send({ status: "ok" })
+        const id = Number.parseInt(req.params.id)
+
+        console.log("TODO: validation token")
+
+        if (isNaN(id)) {
+            return res.status(400).send({ error: "user id must be a number. /users/:id[number]/delete" })
+        }
+
+        const user = await prisma.user.findUnique({
+            where: { id },
+            select: { id: true, firstName: true, secondName: true, email: true }
+        })
+
+        if (!user) {
+            return res.status(404).send({ error: "User not found" })
+        }
+
+        await prisma.user.delete({
+            where: { id }
+        })
+
+        res.send(user)
     }
 }
