7855591946647d8d3249783ad16bd6bff220c6cd
Elias Pinheiro <pinheirinho2004@gmail.com>
2024-01-03 12:13:37 -0300

    minor error fixed

--- prisma/migrations/20240103145239_check_in_confirmation/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `tbl_event` ALTER COLUMN `event_date` DROP DEFAULT;
-
--- AlterTable
-ALTER TABLE `tbl_user_event` ADD COLUMN `check_in` BOOLEAN NOT NULL DEFAULT false;
--- prisma/migrations/20240103151315_adding_token/migration.sql
+++ prisma/migrations/20240103151315_adding_token/migration.sql
@@ -8,4 +8,5 @@
 ALTER TABLE `tbl_event` ALTER COLUMN `event_date` DROP DEFAULT;
 
 -- AlterTable
-ALTER TABLE `tbl_user_event` ADD COLUMN `token` LONGTEXT NOT NULL;
+ALTER TABLE `tbl_user_event` ADD COLUMN `check_in` BOOLEAN NOT NULL DEFAULT false,
+    ADD COLUMN `token` LONGTEXT NOT NULL;
